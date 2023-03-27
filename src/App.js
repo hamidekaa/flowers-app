310b6e848b141d124e79c17c302aa6214b510ec7
hamidekaa <118981041+hamidekaa@users.noreply.github.com>
2023-03-27 20:29:49 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Data from './Data.json';
+import { useState } from 'react';
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Çiçek Sepeti</h1>
+      <div className="inputContainer">
+        <input type="text" className='search' placeholder='Lütfen bir çiçek ismi giriniz:)'
+        onChange={(e)=> setSearchTerm(e.target.value)}/>
+      </div>
+      <div className="dataContainer">
+       {Data.filter((val)=>{
+        if(searchTerm==""){
+          return val;
+        }else if(val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+          return val;
+        }
+       }).map((val)=>{
+        return (
+          <div className='data' key={val.id}>
+            <img src={val.image}/> 
+            <h3> {val.title} </h3>
+            <p> {val.descripton} </p>
+            </div>
+        )
+       })
+       }
+      </div>
     </div>
+    
+
   );
 }
 
